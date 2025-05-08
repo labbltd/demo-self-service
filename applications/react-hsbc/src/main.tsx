3e8c1fc9be669300d7110a9ed8557460a56196f3
Swen <swen.rolink@gmail.com>
2025-05-08 20:12:49 +0200

    Chat for hsbc

--- applications/react-hsbc/src/main.tsx
+++ applications/react-hsbc/src/main.tsx
@@ -1,20 +1,20 @@
-import { TokenInfo } from '@labb/constellation-core-types';
-import { PegaEmbed } from '@labb/react-adapter';
-import { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import HsbcTemplate from '../design-system/hsbc-template';
-import './pega/ContainerMapping';
-import { DemoBootstrap } from '@labb/demo-utilities';
+import { TokenInfo } from "@labb/constellation-core-types";
+import { PegaEmbed } from "@labb/react-adapter";
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import HsbcTemplate from "../design-system/hsbc-template";
+import "./pega/ContainerMapping";
+import { DemoBootstrap } from "@labb/demo-utilities";
+import FloatingChatContainer from "./components/chat/FloatingChatContainer";
+import { ChatProvider } from "./context/ChatContext";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 async function render() {
   try {
-    root.render(
-      <Main token={await DemoBootstrap.getToken()}></Main>
-    );
+    root.render(<Main token={await DemoBootstrap.getToken()}></Main>);
   } catch (error) {
     root.render(null);
   }
@@ -23,17 +23,30 @@ async function render() {
 render();
 
 function Main(props: { token: TokenInfo }) {
-  const [title, setTitle] = useState<string>('');
-  return <HsbcTemplate productImage='public/HSBC_Premier_Debit_Header.png' productName={title}>
-    <PegaEmbed
-      token={props.token}
-      caseTypeID={DemoBootstrap.getCaseTypeId()}
-      serverUrl={DemoBootstrap.getServerUrl()}
-      localeID={DemoBootstrap.getLocaleId()}
-      appID={DemoBootstrap.getAppId()}
-      doneLoading={() => {
-        setTitle(window.PCore.getStore().getState().data['app/primary_1']?.caseInfo?.caseTypeName)
-      }}
-    />
-  </HsbcTemplate >
+  const [title, setTitle] = useState<string>("");
+  return (
+    <>
+      <ChatProvider>
+        <HsbcTemplate
+          productImage="public/HSBC_Premier_Debit_Header.png"
+          productName={title}
+        >
+          <PegaEmbed
+            token={props.token}
+            caseTypeID={DemoBootstrap.getCaseTypeId()}
+            serverUrl={DemoBootstrap.getServerUrl()}
+            localeID={DemoBootstrap.getLocaleId()}
+            appID={DemoBootstrap.getAppId()}
+            doneLoading={() => {
+              setTitle(
+                window.PCore.getStore().getState().data["app/primary_1"]
+                  ?.caseInfo?.caseTypeName
+              );
+            }}
+          />
+        </HsbcTemplate>
+        <FloatingChatContainer />
+      </ChatProvider>
+    </>
+  );
 }
