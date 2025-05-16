7ee8e96e900bb2daa401e6a2efaa0a41c51ff32a
danielwedema-labb <danielwedema-labb@users.noreply.github.com>
2025-05-16 21:16:41 +0000

    deploy: ae3e8c349526dd5b118ba628b4ae7d81d5d6326e

--- assets/Default-Bne2YKlL.js
+++
@@ -1,1 +0,0 @@
-import{j as r,G as a}from"./index-C5iSg-fz.js";function i(n){return n.container.children.map((e,t)=>r.jsx(a,{container:e},e.id+t))}export{i as default};
---
+++ assets/SimpleTableSelect-D102yovE.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BS0kZgz3.js";function o(t){return e.jsx("div",{})}export{o as default};
