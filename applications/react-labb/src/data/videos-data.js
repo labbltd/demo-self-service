56c0ed77605fe23aebccca0a57e9ba5a134ca4e1
phill-jones <phill.jones@labb.ltd>
2025-05-30 21:47:48 +0100

    Add more videos from youtube

--- applications/react-labb/src/data/videos-data.js
+++ applications/react-labb/src/data/videos-data.js
@@ -1,5 +1,14 @@
 export const videos = [
     {
         url: 'https://www.youtube.com/embed/k66mriugoqs?si=gJglONKSPI50Acq_'
+    },
+    {
+        url: 'https://www.youtube.com/embed/fioTHjFnvXs?si=_r_njcqTFX-2miu1'
+    },
+    {
+        url: 'https://www.youtube.com/embed/gRzsALMtAHg?si=mbjyoXyesAsaIypN'
+    },
+    {
+        url: 'https://www.youtube.com/embed/Mxd72y9ZN7M?si=mlZXk79pz3zDxCGx'
     }
 ]
