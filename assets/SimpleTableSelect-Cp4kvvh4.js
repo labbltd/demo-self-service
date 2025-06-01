88d0b06fdeeb4ddfb8964cf6680c185545985e0e
danielwedema-labb <danielwedema-labb@users.noreply.github.com>
2025-06-01 11:55:57 +0000

    deploy: fc492fdc353718fd12523e5a4b9bfccd9bd065cf

---
+++ assets/SimpleTableSelect-Cp4kvvh4.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DS7Jb4gj.js";function o(t){return e.jsx("div",{})}export{o as default};
--- assets/SimpleTableSelect-DZGUmYPw.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DM-hBvPb.js";function o(t){return e.jsx("div",{})}export{o as default};
