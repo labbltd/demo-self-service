222e48128323d335fab4e1762b3abb20c8bdb4a7
labb-rolis <labb-rolis@users.noreply.github.com>
2025-07-10 09:54:56 +0000

    deploy: 3e150c99ced0b5376eff3cae503033eca6da65b2

---
+++ assets/SimpleTableSelect-XLBpOHKa.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DVez17aK.js";function o(t){return e.jsx("div",{})}export{o as default};
--- assets/SimpleTableSelect-zjX9jAgl.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-C4zft3cN.js";function o(t){return e.jsx("div",{})}export{o as default};
