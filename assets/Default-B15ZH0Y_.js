19b370cb47e21e1b3b2de842585f518d3d68ae6e
danielwedema-labb <danielwedema-labb@users.noreply.github.com>
2025-09-19 07:09:22 +0000

    deploy: f37318a15ab8d962107361c7ac603f66ac6283ca

--- assets/Default-B15ZH0Y_.js
+++
@@ -1,1 +0,0 @@
-import{j as r,G as a}from"./index-DgRdZg9_.js";function i(n){return n.container.children.map((e,t)=>r.jsx(a,{container:e},e.id+t))}export{i as default};
---
+++ assets/SimpleTableSelect-DbT9Ug2-.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DnOAZOUR.js";function o(t){return e.jsx("div",{})}export{o as default};
